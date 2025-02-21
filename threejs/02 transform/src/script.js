db7032f5ad14d879f7b9263fc78dd3a01eb6ba7e
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2025-02-21 16:32:03 +0200

    upd

--- threejs/02 transform/src/script.js
+++ threejs/02 transform/src/script.js
@@ -9,10 +9,36 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x -= 2
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x += 2
+
+
+const group = new THREE.Group()
+group.position.y -= 1
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+scene.add(group)
+
+
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -26,11 +52,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0, 1, 3)
+camera.position.set(0, 1, 5)
 scene.add(camera)
 
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
 
 /**
  * Renderer
