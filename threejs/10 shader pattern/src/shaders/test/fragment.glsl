e9cae764b2c49a4ca9b1bc2c9e774178bc340d7d
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2025-10-15 23:14:31 +0300

    Update fragment.glsl

--- threejs/10 shader pattern/src/shaders/test/fragment.glsl
+++ threejs/10 shader pattern/src/shaders/test/fragment.glsl
@@ -8,6 +8,7 @@ void main() {
     // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
     // vec2 coord = vec2(floor(vUv.x * 10.0) / 10.0, floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
     // float strength = rand(coord);
-    float strength = 1.0 - length(vUv - 0.5);
+    // float strength = 1.0 - length(vUv - 0.5);
+    float strength = 0.015 / distance( vec2(0.5), vUv );
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
